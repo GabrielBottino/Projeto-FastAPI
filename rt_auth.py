ba6ef2413ce0fe2802395ef1284e84b98e681ff0
GabrielBottino <gabrielanjosbottino@gmail.com>
2026-08-04 21:16:20 -0300

    Criando a primeira rota de autenticação

--- rt_auth.py
+++ rt_auth.py
@@ -1,10 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from models import Usuario
+from dependencies import get_session
 
 auth_rt = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_rt.get("/")
-async def autorizacao():
+async def home():
     """
     Endpoint padrão bixo
     """
     return {"mensagem": "Você está acessando a rota de autorização"}
+
+@auth_rt.post("/criar_conta")
+async def criar_conta(email: str, senha: str, nome: str, session=Depends(get_session)):
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    if usuario:
+        return {"mensagem": "Já existe um usuário no email"}
+    else:
+        novo_usuario = Usuario(nome, email, senha)
+        session.add(novo_usuario)
+        session.commit()
+        return {"mensagem": "usuário cadstrado com sucesso"}
