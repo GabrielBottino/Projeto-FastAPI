0c6d5bd50096054be2213d00d56ee065c570c622
GabrielBottino <gabrielanjosbottino@gmail.com>
2026-08-15 21:54:21 -0300

    Endpoints'adicionar_item', 'finalizar_pedido','visualizar_pedido', 'listar_pedidos' criados

--- rt_order.py
+++ rt_order.py
@@ -22,7 +22,7 @@ async def cancelar_pedido(id_pedido: int, session=Depends(get_session), usuario:
     pedido = session.query(Pedido).filter(Pedido.id==id_pedido).first()
     if not pedido:
         raise HTTPException(status_code=400, detail="Pedido não encontrado.")
-    if not usuario.adm or usuario.id != pedido.usuario:
+    if not usuario.admin and usuario.id != pedido.usuario:
         raise HTTPException(status_code=400, detail="O usuário não tem permissão para editar esse pedido.")
     pedido.status = "CANCELADO"
     session.commit()
@@ -61,3 +61,54 @@ async def adicionar_item(id_pedido: int, item_pedido_scheme: item_pedido_scheme,
     return {"mensagem": "Item criado",
             "id_item": item_pedido.id,
             "preco_pedido": pedido.preco}
+
+@order_rt.post("/pedido/remover-item/{id_item}")
+async def adicionar_item(id_item: int,session= Depends(get_session), usuario: Usuario = Depends(verificar_token)):
+    item = session.query(ItemPedido).filter(ItemPedido.id==id_item).first()
+    if not item:
+        raise HTTPException(status_code=401, detail="Item não encontrado não encontrado.")
+    pedido = session.query(Pedido).filter(Pedido.id==item.pedido).first()
+    if usuario.id != pedido.usuario and not usuario.admin:
+        raise HTTPException(status_code=401, detail="Usuário não tem permissão de remover item.")
+
+    session.delete(item)
+    pedido.calcular_preco()
+    session.commit()
+    return {"mensagem": "Item removido",
+            "preco_pedido": pedido.preco,
+            "pedido_itens": len(pedido.itens),
+            "pedido": pedido}
+
+@order_rt.post("/pedido/finalizar/{id_pedido}")
+async def finalizar_pedido(id_pedido: int, session=Depends(get_session), usuario: Usuario = Depends(verificar_token)):
+    pedido = session.query(Pedido).filter(Pedido.id==id_pedido).first()
+    if not pedido:
+        raise HTTPException(status_code=400, detail="Pedido não encontrado.")
+    if not usuario.admin or usuario.id != pedido.usuario:
+        raise HTTPException(status_code=400, detail="O usuário não tem permissão para editar esse pedido.")
+    pedido.status = "FINALIZADO"
+    session.commit()
+    return {
+        "mensagem": f"Pedido com id {pedido.id} cancelado com sucesso",
+        "pedido": pedido
+    }
+@order_rt.get("/pedido/visualizar/{id_pedido}")
+async def visualizar_pedido(id_pedido: int, session=Depends(get_session), usuario: Usuario = Depends(verificar_token)):
+    pedido = session.query(Pedido).filter(Pedido.id==id_pedido).first()
+    if not pedido:
+        raise HTTPException(status_code=400, detail="Pedido não encontrado.")
+    elif not usuario.admin and usuario.id != pedido.usuario:
+        raise HTTPException(status_code=401, detail="O usuário não tem permissão para visualizar esse pedido.")
+    return {
+        "id": pedido.id,
+        "quantidade_itens_pedido": len(pedido.itens),
+        "pedido": pedido
+    }
+
+@order_rt.get("/listar/pedidos-usuario")
+async def listar_pedidos(session= Depends(get_session), usuario: Usuario = Depends(verificar_token)):
+    pedidos = session.query(Pedido).filter(Pedido.usuario==usuario.id).all()
+    return {
+        "quantidade_pedidos": len(pedidos),
+        "pedidos": pedidos
+    }
